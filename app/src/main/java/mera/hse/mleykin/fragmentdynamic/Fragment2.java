726f1da207f69b1a9986f1ddd0242ca1f897b085
mc_off <makov.tm@gmail.com>
2019-10-28 15:35:53 +0300

    Fix error with pushing "Add" button two times or more Fix bug with removing after overlay of two fragments

--- app/src/main/java/mera/hse/mleykin/fragmentdynamic/Fragment2.java
+++ app/src/main/java/mera/hse/mleykin/fragmentdynamic/Fragment2.java
@@ -2,14 +2,19 @@ package mera.hse.mleykin.fragmentdynamic;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class Fragment2 extends Fragment {
+    final String LOG_TAG = "myLogs";
 
-      public View onCreateView(LayoutInflater inflater, ViewGroup container,
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Log.d(LOG_TAG, "Fragment2 onCreateView");
+
         return inflater.inflate(R.layout.fragment2, null) ;
     }
 
--- app/src/main/java/mera/hse/mleykin/fragmentdynamic/MainActivity.java
+++ app/src/main/java/mera/hse/mleykin/fragmentdynamic/MainActivity.java
@@ -4,8 +4,11 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
+
+import java.util.List;
 
 
 public class MainActivity extends Activity {
@@ -16,6 +19,8 @@ public class MainActivity extends Activity {
     FragmentTransaction fTrans;
     CheckBox chbStack;
     StringBuilder sb;
+    final String LOG_TAG = "myLogs";
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,16 +31,30 @@ public class MainActivity extends Activity {
     }
 
     public void onClick(View v) {
+        List<Fragment> lFragm = getFragmentManager().getFragments();
         fTrans = getFragmentManager().beginTransaction();
         Fragment fragment = getFragmentManager().findFragmentByTag("R.id.Fragment1");
+        for (Fragment fragm:lFragm) {
+            System.out.println(fragm.getTag());
+        }
 
         switch (v.getId()) {
             case R.id.btnAdd:
-                fTrans.add(R.id.frgmCont, frag1);
-                current = frag1;
-                fTrans.commit();
-                break;
+                if (frag1.isAdded()) {
+                    break;
+                } else {
+                    fTrans.add(R.id.frgmCont, frag1);
+                    Log.d(LOG_TAG, "Try to add");
+                    current = frag1;
+                    fTrans.commit();
+                    break;
+                }
             case R.id.btnRemove:
+                if (frag1.isVisible())
+                    current = frag1;
+                else
+                if (frag2.isVisible())
+                    current = frag2;
                 fTrans.remove(current);
                 fTrans.commit();
                 break;
